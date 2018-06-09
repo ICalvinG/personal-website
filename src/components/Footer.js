39e10b4d1eeee703ec9d298f7c9492d1de9fdcdd
Calvin Ieong <calvinieong1118@gmail.com>
2018-06-09 16:10:29 +0200

    Add links to Icons and extra styling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // Components
 import Icon from './Icon.js';
 
-const Footer = () => (
-  <footer className="footer__wrapper">
-    <Icon
-      brand={"b"}
-      type={"github"}
-    />
-    <Icon
-      type={"envelope"}
-    />
-    <Icon
-      brand={"b"}
-      type={"linkedin-in"}
-    />
-  </footer>
-);
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      github: "https://github.com/icalving",
+      email: "mailto:calvinieong1118@gmail.com",
+      linkedin: "https://www.linkedin.com/in/calvinieong",
+    };
+  }
+
+  render() {
+
+    const {
+      github,
+      email,
+      linkedin,
+     } = this.state;
+
+    return (
+      <footer className="footer__wrapper">
+        <Icon
+          brand={"b"}
+          type={"github"}
+          link={github}
+        />
+        <Icon
+          type={"envelope"}
+          link={email}
+        />
+        <Icon
+          brand={"b"}
+          type={"linkedin-in"}
+          link={linkedin}
+        />
+      </footer>
+    );
+  };
+};
 
 export default Footer;
--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Icon = ({ brand, type }) => (
-  <i className={`fa${brand ? 'b' : 's'} fa-${type} fa-3x`}></i>
+const Icon = ({
+  brand,
+  type,
+  link,
+}) => (
+  <a
+    className="icon__link"
+    href={link}>
+    <i
+      className={`fa${brand ? 'b' : 's'} fa-${type} fa-3x`}>
+    </i>
+  </a>
 );
 
 Icon.propTypes = {
   brand:  PropTypes.string,
+  type:   PropTypes.string.isRequired,
   type:   PropTypes.string.isRequired,
 };
 

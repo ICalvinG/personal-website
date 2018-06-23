269f30c0c8d8988fbe160c7cfc8f20ebd3ed2e1b
Calvin Ieong <calvinieong1118@gmail.com>
2018-06-23 14:48:43 +0200

    Add about me

--- src/components/About.js
+++ src/components/About.js
@@ -2,13 +2,29 @@ import React from 'react';
 
 const About = () => (
   <section className='about__section'>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-    Vestibulum facilisis ut neque vulputate lobortis.
-    Suspendisse in dapibus purus. Praesent molestie eget elit a
-    lobortis. Vivamus interdum tincidunt nisi ac bibendum.
-    Nullam erat nulla, imperdiet in efficitur id, sagittis a mauris.
-    Suspendisse auctor leo eget dolor accumsan, id rhoncus leo pulvinar.
-    Fusce sapien risus, cursus in feugiat id, euismod vitae enim.
+    <p>
+      Hello, my name is Calvin. I was born and raised in Brooklyn, New York
+      in a neighborhood called Bensonhurst. I enjoy listening to
+      <a href="https://soundcloud.com/icalving"> music</a>, watching tv
+      shows/movies, reading manga/watching anime, playing video games, and
+      writing <a href="https://github.com/ICalvinG"> code</a>.
+    </p>
+
+    <p>
+      One odd fact about me is that I tend to have a endless void of trivial
+      knowledge as I happen to fall into the wikipedia rabbit hole easily
+      losing hours of my time. This does work in my favor because it means I
+      can be conversational about almost anything and very good to have on
+      your trivia night team.
+    </p>
+
+    <p>
+      One of my favorite things to do is to travel because I embrace the feeling
+      of not knowing and there is just so much in this world to discover. My
+      stomach and taste buds also thank me because I believe one of the quickest
+      ways to learn another culture is through food.
+    </p>
+
   </section>
 );
 

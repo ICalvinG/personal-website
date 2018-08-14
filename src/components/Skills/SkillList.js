74021adfe940ec3413f07d99d7277252da5483c8
ICalvinG <calvinieong1118@gmail.com>
2018-08-14 14:33:39 -0400

    Add skills to skill list

--- src/components/Skills/SkillList.js
+++ src/components/Skills/SkillList.js
@@ -15,7 +15,7 @@ const SkillList = () => (
     />
     <SkillIcon
       type={"javascript"}
-      level={3}
+      level={5}
     />
     <SkillIcon
       type={"css3"}
@@ -24,6 +24,66 @@ const SkillList = () => (
     <SkillIcon
       type={"html5"}
       level={5}
+    />
+    <SkillIcon
+      type={"react"}
+      level={5}
+    />
+    <SkillIcon
+      type={"git"}
+      level={5}
+    />
+    <SkillIcon
+      type={"github"}
+      level={5}
+    />
+    <SkillIcon
+      type={"heroku"}
+      level={3}
+    />
+    <SkillIcon
+      type={"jquery"}
+      level={5}
+    />
+    <SkillIcon
+      type={"mysql"}
+      level={3}
+    />
+    <SkillIcon
+      type={"postgresql"}
+      level={3}
+    />
+    <SkillIcon
+      type={"linux"}
+      level={3}
+    />
+    <SkillIcon
+      type={"sass"}
+      level={4}
+    />
+    <SkillIcon
+      type={"vim"}
+      level={2}
+    />
+    <SkillIcon
+      type={"wordpress"}
+      level={2}
+    />
+    <SkillIcon
+      type={"travis"}
+      level={3}
+    />
+    <SkillIcon
+      type={"webpack"}
+      level={2}
+    />
+    <SkillIcon
+      type={"slack"}
+      level={5}
+    />
+    <SkillIcon
+      type={"ssh"}
+      level={3}
     />
   </div>
 );

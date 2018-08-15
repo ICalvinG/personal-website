6188665e7224ef2eb1b179ccde94044b23f4c2fd
ICalvinG <calvinieong1118@gmail.com>
2018-08-15 17:42:55 -0400

    Add Mobile Navigation along with onClick logic

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // Components
+import MobileNavigation from './MobileNavigation.js';
 import Navigation from './Navigation.js';
 
-const Header = () => (
-  <header>
-    <i className="fa fa-bars header__navigation-icon" aria-hidden="true"></i>
-    <div className='header__title'>
-      <p>Calvin C. Ieong</p>
+class Header extends Component {
+  constructor() {
+    super();
 
-      <div className='header__description'>
-        <p>
-          I am a full-stack developer and I want to be the very best,
-          like no one ever was.
-        </p>
+    this.state = {
+      showMenu: false,
+    };
 
-        <Navigation />
-      </div>
-    </div>
-  </header>
-);
+    this.showMenu = this.showMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+  }
+
+  showMenu(event) {
+    event.preventDefault();
+
+    this.setState({ showMenu: true }, () => {
+      document.addEventListener('click', this.closeMenu);
+    });
+  }
+
+  closeMenu(event) {
+
+    if (!this.dropdownMenu.contains(event.target)) {
+
+      this.setState({ showMenu: false }, () => {
+        document.removeEventListener('click', this.closeMenu);
+      });
+
+    }
+  }
+
+  render() {
+    return (
+      <header>
+        <i
+          className="fa fa-bars header__navigation-icon"
+          aria-hidden="true"
+          onClick={this.showMenu}
+        ></i>
+
+        {
+          this.state.showMenu
+            ? (
+              <div ref={(element) => {
+                  this.dropdownMenu = element;
+                }}
+              >
+                <MobileNavigation />
+              </div>
+            )
+            : (
+              null
+            )
+        }
+
+        <div className='header__title'>
+          <p>Calvin C. Ieong</p>
+
+          <div className='header__description'>
+            <p>
+              I am a full-stack developer and I want to be the very best,
+              like no one ever was.
+            </p>
+
+            <Navigation />
+          </div>
+        </div>
+      </header>
+    );
+  }
+}
 
 export default Header;

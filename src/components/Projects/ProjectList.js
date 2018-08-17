ca191e1d00caaa5b26e077ceb076482c9e62eda0
ICalvinG <calvinieong1118@gmail.com>
2018-08-16 20:47:47 -0400

    Create stylesheet for ProjectList and add grid styling

--- src/components/Projects/ProjectList.js
+++ src/components/Projects/ProjectList.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 // Components
 import SectionHeader from '../SectionHeader.js';
+import Project from '../Projects/Project.js';
 
 const ProjectList = () => (
   <section>
     <SectionHeader title={"Projects"} />
+
+    <div className="project__list-container">
+      <Project />
+      <Project />
+    </div>
   </section>
 );
 
